b7ee9e0ce906474873e012c5e684e908de577074
Robert Mordzon <robertmordzon@gmail.com>
2024-04-26 11:09:13 +0200

    Delete distros/raspberry_pi/geth directory

--- distros/raspberry_pi/geth/geth.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-geth --authrpc.port=8551 --http --http.port 8545 --http.addr 0.0.0.0 --http.vhosts '*' --ws --ws.port 8546 --ws.addr 0.0.0.0 --ws.origins '*' --authrpc.jwtsecret /home/ethereum/clients/secrets/jwt.hex --state.scheme=path --discovery.port 30303 --port 30303
