7263c03711c0483165edafefb6cee18124071403
Robert Mordzon <robertmordzon@gmail.com>
2024-04-26 15:03:53 +0200

    Update nimbus.sh

--- distros/raspberry_pi/rpi5/nimbus/nimbus.sh
+++ distros/raspberry_pi/rpi5/nimbus/nimbus.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# STAGE 1 - trustedNodeSync
+nimbus_beacon_node trustedNodeSync --network:mainnet --data-dir=/home/ethereum/.nimbus/data/shared_mainnet_0 --trusted-node-url=http://18.194.243.122:30307
+# STAHE 2 - work mode
 nimbus_beacon_node --network:mainnet --data-dir=/home/ethereum/.nimbus/data/shared_mainnet_0 --jwt-secret=/home/ethereum/clients/secrets/jwt.hex --el=http://127.0.0.1:8551 --tcp-port=9000 --udp-port=9000 --rest=true --rest-port=5052 --rest-address=0.0.0.0 --rest-allow-origin='*'
