a2b6ab029a5d7e3b4e9b09fce2072377245f40e7
Robert Mordzon <robertmordzon@gmail.com>
2024-05-06 15:25:11 +0200

    Update nimbus.sh

--- distros/raspberry_pi/rpi5/nimbus/nimbus.sh
+++ distros/raspberry_pi/rpi5/nimbus/nimbus.sh
@@ -10,9 +10,28 @@ config_get() {
     printf -- "%s" "${val}";
 }
 
+
 # STAGE 1 - trustedNodeSync
 tn_url="$(config_get nimbus-trusted-node-url)";
 
+# Checking internet connection
+echo "Checking internet connection"
+
+pingServerAdr="github.com"
+ping_n=0
+ping_max=10
+
+ping -c 1 $pingServerAdr > /dev/null 2>&1
+while [ $? -ne 0 ]; do
+  echo -e "\e[1A\e[K $(date): test connection [$ping_n/$ping_max] - ${pingServerAdr}"
+  sleep 6
+  let "ping_n+=1"
+  [[ ${ping_n} -gt ${ping_max} ]] && echo "Stopping the installation, internet access is necessary" && exit 1
+  ping -c 1 $pingServerAdr > /dev/null 2>&1
+done
+
+echo "$(date): Connected - ${pingServerAdr}"
+  
 if [ "$(config_get nimbus-run-mode)" = "full_sync" ]; then
   nimbus_beacon_node trustedNodeSync --network:mainnet --data-dir=/home/ethereum/.nimbus/data/shared_mainnet_0 --trusted-node-url=${tn_url}
 fi
