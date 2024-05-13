2dcae860dce605f873a5f995b268b36977cdbb95
Robert Mordzon <robertmordzon@gmail.com>
2024-05-13 07:55:02 +0200

    Update nimbus_light.sh

--- distros/raspberry_pi/rpi5/nimbus/nimbus_light.sh
+++ distros/raspberry_pi/rpi5/nimbus/nimbus_light.sh
@@ -1,3 +1,5 @@
-TRUSTED_BLOCK_ROOT=$(curl -s "http://testing.mainnet.beacon-api.nimbus.team/eth/v1/beacon/headers/finalized" | jq -r '.data.root')
+# testing
 
-nimbus_light_client --web3-url=http://127.0.0.1:8551 --jwt-secret=/home/ethereum/clients/secrets/jwt.hex --trusted-block-root=$TRUSTED_BLOCK_ROOT --data-dir=/home/ethereum/.nimbus/data/shared_mainnet_0 --tcp-port=9000 --udp-port=9000 --rest=true --rest-port=5052 --rest-address=0.0.0.0 --rest-allow-origin='*'
+#TRUSTED_BLOCK_ROOT=$(curl -s "http://testing.mainnet.beacon-api.nimbus.team/eth/v1/beacon/headers/finalized" | jq -r '.data.root')
+
+#nimbus_light_client --web3-url=http://127.0.0.1:8551 --jwt-secret=/home/ethereum/clients/secrets/jwt.hex --trusted-block-root=$TRUSTED_BLOCK_ROOT --data-dir=/home/ethereum/.nimbus/data/shared_mainnet_0 --tcp-port=9000 --udp-port=9000 --rest=true --rest-port=5052 --rest-address=0.0.0.0 --rest-allow-origin='*'
