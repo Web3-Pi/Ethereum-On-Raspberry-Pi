16d8420ddda344e5030f83bd0ea5354980f298e8
Robert Mordzon <robertmordzon@gmail.com>
2024-05-13 07:59:16 +0200

    Update nimbus.sh

--- distros/raspberry_pi/rpi5/nimbus/nimbus.sh
+++ distros/raspberry_pi/rpi5/nimbus/nimbus.sh
@@ -13,7 +13,7 @@ config_get() {
 
 # STAGE 1 - trustedNodeSync
 tn_url="$(config_get nimbus-trusted-node-url)";
-
+el_url="$(config_get exec-url)";
 # Checking internet connection
 echo "Checking internet connection"
 
@@ -42,4 +42,4 @@ fi
 
 
 # STAGE 2 - work mode
-nimbus_beacon_node --network:mainnet --data-dir=/home/ethereum/.nimbus/data/shared_mainnet_0 --jwt-secret=/home/ethereum/clients/secrets/jwt.hex --el=http://127.0.0.1:8551 --tcp-port=9000 --udp-port=9000 --rest=true --rest-port=5052 --rest-address=0.0.0.0 --rest-allow-origin='*'
+nimbus_beacon_node --network:mainnet --data-dir=/home/ethereum/.nimbus/data/shared_mainnet_0 --jwt-secret=/home/ethereum/clients/secrets/jwt.hex --el=${el_url} --tcp-port=9000 --udp-port=9000 --rest=true --rest-port=5052 --rest-address=0.0.0.0 --rest-allow-origin='*'
