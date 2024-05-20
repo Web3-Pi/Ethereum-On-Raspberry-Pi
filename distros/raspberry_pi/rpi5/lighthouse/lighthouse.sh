acd7a61b8ba11a2a28f386c68f2db03c1fa41690
Robert Mordzon <robertmordzon@gmail.com>
2024-05-20 23:02:48 +0200

    Update lighthouse.sh
    --execution-endpoint ${el_url}

--- distros/raspberry_pi/rpi5/lighthouse/lighthouse.sh
+++ distros/raspberry_pi/rpi5/lighthouse/lighthouse.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-lighthouse bn --network mainnet --execution-endpoint http://localhost:8551 --execution-jwt /home/ethereum/clients/secrets/jwt.hex --checkpoint-sync-url https://mainnet.checkpoint.sigp.io --disable-deposit-contract-sync --http --http-port 5052 --http-address=0.0.0.0 --port 9000
+
+# Read custom config flags from /boot/firmware/config.txt
+config_read_file() {
+    (grep -E "^${2}=" -m 1 "${1}" 2>/dev/null || echo "VAR=UNDEFINED") | head -n 1 | cut -d '=' -f 2-;
+}
+
+config_get() {
+    val="$(config_read_file /boot/firmware/config.txt "${1}")";
+    printf -- "%s" "${val}";
+}
+
+
+el_url="$(config_get exec-url)";
+
+
+lighthouse bn --network mainnet --execution-endpoint ${el_url} --execution-jwt /home/ethereum/clients/secrets/jwt.hex --checkpoint-sync-url https://mainnet.checkpoint.sigp.io --disable-deposit-contract-sync --http --http-port 5052 --http-address=0.0.0.0 --port 9000
