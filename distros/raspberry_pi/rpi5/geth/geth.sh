cc71088cbfa64aa56779955a09aeac4c65289c38
Robert Mordzon <robertmordzon@gmail.com>
2024-05-23 11:23:43 +0200

    Update geth.sh

--- distros/raspberry_pi/rpi5/geth/geth.sh
+++ distros/raspberry_pi/rpi5/geth/geth.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-geth --authrpc.addr=0.0.0.0 --authrpc.port 8551 --authrpc.vhosts=* --authrpc.jwtsecret /home/ethereum/clients/secrets/jwt.hex --http --http.addr 0.0.0.0 --http.vhosts=* --http.api eth,net,web3 --state.scheme=path --ws --ws.port 8546 --ws.addr 0.0.0.0 --ws.origins '*'
+geth --authrpc.addr 0.0.0.0 --authrpc.vhosts '*' --authrpc.port 8551 --authrpc.jwtsecret /home/ethereum/clients/secrets/jwt.hex --ws --ws.addr 0.0.0.0 --ws.origins '*' --ws.port 8546 --http --http.addr 0.0.0.0 --http.vhosts '*' --http.port 8545 --state.scheme=path --discovery.port 30303 --port 30303
