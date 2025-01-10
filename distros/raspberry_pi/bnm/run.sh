4bac36128e380be8fb6d4b1cb99b5f3d7e3906c1
Robert Mordzon <robertmordzon@gmail.com>
2025-01-10 21:37:52 +0100

    No 2 min delay before basic-eth2-node-monitor start

--- distros/raspberry_pi/bnm/run.sh
+++ distros/raspberry_pi/bnm/run.sh
@@ -5,8 +5,8 @@ cd /opt/web3pi/basic-eth2-node-monitor
 source "venv/bin/activate"
 python3 -m pip install -r requirements.txt
 
-echo "Delay of 2 minutes before start"
-sleep 2m
+#echo "Delay of 2 minutes before start"
+#sleep 2m
 
 # Single node
 python3 -u nodemonitor.py -sn "${HOSTNAME}.local" d -db localhost
